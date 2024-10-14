b7d985d5944d35a1238e3f5ef1d3f1df74657b88
Miguel Caeiro <miguel.caeiro@openbet.com>
2024-10-14 13:52:53 +0100

    Search for correct container name to attach too or provide error message

--- docker_attach.sh
+++ docker_attach.sh
@@ -1,1 +1,7 @@
-docker exec -it askgiles /bin/bash
+name=$(docker ps | grep askgiles | grep Up | cut -d" " -f1) 
+
+if [ -n "$name" ]; then
+     docker exec -it ${name} /bin/bash
+else
+     echo "No running askgiles container was found, check with > docker -ps"
+fi
